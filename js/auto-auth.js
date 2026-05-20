785a61358967bc7845ad8a484e06967b47268c79
yamada@danmaid.com <yamada@danmaid.com>
2026-05-20 11:48:40 +0900

    ごちゃごちゃ

---
+++ js/auto-auth.js
@@ -0,0 +1,26 @@
+const credentials = new class Credentials extends EventTarget {
+  get(key) {
+    return localStorage.getItem('credentials.' + key)
+  }
+  set(key, value) {
+    localStorage.setItem('credentials.' + key, value)
+    this.dispatchEvent(new CustomEvent('change', { detail: { key } }))
+  }
+}
+
+/** @type {Record<string, (url: URL, args: Parameters<typeof fetch>) => void>} */
+const handlers = {
+  'http://localhost:8080': async (url, args) => {
+    // ここで認証などなど
+  }
+}
+
+const proxy = new Proxy(window.fetch, {
+  async apply(target, thisArg, args) {
+    const [input] = args
+    const url = new URL(input?.url ?? input, location.href)
+    await handlers[url.origin]?.(url, args)
+    return Reflect.apply(target, thisArg, args)
+  }
+})
+window.fetch = proxy
