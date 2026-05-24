70affea70482c2c867b3bb277be0fa9249c1af22
yamada@danmaid.com <yamada@danmaid.com>
2026-05-24 16:12:07 +0900

    test

--- js/auto-auth.js
+++ js/auto-auth.js
@@ -1,26 +1,30 @@
-const credentials = new class Credentials extends EventTarget {
-  get(key) {
-    return localStorage.getItem('credentials.' + key)
+{
+  const credentials = new class Credentials extends EventTarget {
+    get(key) {
+      return localStorage.getItem('credentials.' + key)
+    }
+    set(key, value) {
+      localStorage.setItem('credentials.' + key, value)
+      this.dispatchEvent(new CustomEvent('change', { detail: { key } }))
+    }
+    has(key) {
+      return localStorage.getItem('credentials.' + key) !== null
+    }
   }
-  set(key, value) {
-    localStorage.setItem('credentials.' + key, value)
-    this.dispatchEvent(new CustomEvent('change', { detail: { key } }))
-  }
-}
 
-/** @type {Record<string, (url: URL, args: Parameters<typeof fetch>) => void>} */
-const handlers = {
-  'http://localhost:8080': async (url, args) => {
-    // ここで認証などなど
+  /** @type {Record<string, (url: URL, args: Parameters<typeof fetch>) => void>} */
+  const handlers = {
+    'http://localhost:8080': async (url, args) => {
+      // ここで認証などなど
+    }
   }
+  const proxy = new Proxy(window.fetch, {
+    async apply(target, thisArg, args) {
+      const [input] = args
+      const url = new URL(input?.url ?? input, location.href)
+      await handlers[url.origin]?.(url, args)
+      return Reflect.apply(target, thisArg, args)
+    }
+  })
+  globalThis.fetch = proxy
 }
-
-const proxy = new Proxy(window.fetch, {
-  async apply(target, thisArg, args) {
-    const [input] = args
-    const url = new URL(input?.url ?? input, location.href)
-    await handlers[url.origin]?.(url, args)
-    return Reflect.apply(target, thisArg, args)
-  }
-})
-window.fetch = proxy
