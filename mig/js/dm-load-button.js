01d7ade17a28ac0c77a31718cea1c3ee8a0d2b53
yamada@danmaid.com <yamada@danmaid.com>
2026-06-15 14:21:56 +0000

    mig ツールズ

---
+++ mig/js/dm-load-button.js
@@ -0,0 +1,66 @@
+class DmLoadButton extends HTMLElement {
+  static observedAttributes = ['value', 'url']
+  static css = new CSSStyleSheet()
+  static template = document.createElement('template')
+  static {
+    this.template.innerHTML = `
+          <button>
+            <slot>
+              <slot name="title">Load</slot>
+              <progress style="display: none;"></progress>
+              <span></span>
+            </slot>
+          </button>  
+        `
+  }
+  raw = null
+
+  set value(v) { this.setAttribute('value', v) }
+  get value() { return this.getAttribute('value') }
+
+  set url(v) { this.setAttribute('url', v) }
+  get url() {
+    return this.getAttribute('url').includes('{value}')
+      ? this.value && this.getAttribute('url').replace('{value}', this.value)
+      : this.getAttribute('url')
+  }
+
+  constructor() {
+    super()
+    const shadow = this.attachShadow({ mode: 'open' })
+    shadow.adoptedStyleSheets = [DmLoadButton.css]
+    shadow.appendChild(DmLoadButton.template.content.cloneNode(true))
+    this.button = shadow.querySelector('button')
+    this.progress = shadow.querySelector('progress')
+    this.result = shadow.querySelector('span')
+    this.button.addEventListener('click', () => this.execute())
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue === newValue) return
+    if (name === 'url' || name === 'value') return this.execute()
+  }
+
+  // 👉 差し替えポイント
+  async fetch() {
+    return fetch(this.url)
+  }
+
+  async execute() {
+    if (!this.url) return
+    try {
+      this.progress.style.display = "initial"
+      this.raw = await this.fetch()
+      this.result.textContent = new Date().toLocaleTimeString()
+      this.dispatchEvent(new Event('change', { bubbles: true }))
+    } catch (error) {
+      console.error(error)
+      this.raw = null
+      this.dispatchEvent(new ErrorEvent('error', { bubbles: true, error }))
+    } finally {
+      this.progress.style.display = "none"
+    }
+  }
+}
+
+customElements.define('dm-load-button', DmLoadButton)
---
+++ mig/js/dm-sdwan.js
@@ -0,0 +1,57 @@
+customElements.define('dm-sdwan', class DmSdwan extends HTMLElement {
+  static observedAttributes = ['manager']
+  #manager = ''
+  get manager() { return this.#manager }
+  token = ''
+  cred
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'manager') return this.#manager = newValue
+  }
+
+  constructor() {
+    super()
+    const shadow = this.attachShadow({ mode: 'open' })
+    shadow.innerHTML = `
+      <label>username: <input type="text" id="user"></label>
+      <label>password: <input type="password" id="pass"></label>
+      <button id="set">SET</button>
+    `
+    const user = shadow.querySelector('#user')
+    const pass = shadow.querySelector('#pass')
+    this.cred = JSON.parse(localStorage.getItem('dm-sdwan'))
+    if (this.cred) {
+      user.value = this.cred.j_username
+      pass.value = '************'
+    }
+    shadow.querySelector('#set').addEventListener('click', () => {
+      this.cred = { j_username: user.value, j_password: pass.value }
+      localStorage.setItem('dm-sdwan', JSON.stringify(this.cred))
+    })
+  }
+
+  /** @type {(path: string, init?: RequestInit) => Promise<Response>} */
+  async fetch(path, init, ctx = {}) {
+    const url = this.manager + '/dataservice' + path
+    const headers = new Headers(init?.headers)
+    if (!headers.has('X-XSRF-Token')) headers.set('X-XSRF-Token', this.token)
+    if (!headers.has('Content-Type')) headers.set('Content-Type', 'application/json')
+    const res = await fetch(url, { ...init, headers, credentials: 'include' })
+    if (res.headers.get('Content-Type')?.includes('text/html')) {
+      if (!ctx?.retry) return this.auth().then(() => this.fetch(path, init, { retry: true }))
+    }
+    return res
+  }
+
+  async auth() {
+    const r = await fetch(this.manager + '/dataservice/j_security_check', {
+      method: 'POST',
+      body: new URLSearchParams(this.cred),
+      credentials: 'include'
+    })
+    if (!r.ok) throw r
+    this.token = await fetch(this.manager + '/dataservice/client/token', {
+      credentials: 'include'
+    }).then(r => r.text())
+  }
+})
