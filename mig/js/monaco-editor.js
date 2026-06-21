cf8870c8800da6ca07c4b49e2716e184eaae55f3
yamada@danmaid.com <yamada@danmaid.com>
2026-06-21 15:50:27 +0900

    方向性

--- mig/js/monaco-editor.js
+++ mig/js/monaco-editor.js
@@ -1,21 +1,3 @@
-// 共通
-class MonacoElement extends HTMLElement {
-  static config = Symbol('config')
-  static ready = new Promise((r) => globalThis.require(['vs/editor/editor.main'], r))
-
-  editorReady
-  editor
-  constructor() {
-    super()
-    if (!globalThis.require) throw Error("Monaco Editor is not loaded. Please include loader.js and configure require paths.")
-    this.editorReady = MonacoElement.ready
-      .then(() => this.initialize())
-      .then(() => this.dispatchEvent(new Event("ready", { bubbles: true, composed: true })))
-  }
-
-  async initialize() { }
-}
-
 /**
  * Monaco Editor Web Component
  * <script src="vendor/vs/loader.js"></script>
@@ -26,14 +8,70 @@ class MonacoElement extends HTMLElement {
  * <monaco-editor target="#xxx" diff="#yyy"></monaco-editor>
  * <monaco-editor language="json"></monaco-editor>
  */
-customElements.define("monaco-editor", class MonacoEditor extends MonacoElement {
-  async initialize() {
-    this.editor = monaco.editor.create(this, {
+customElements.define("monaco-editor", class MonacoEditor extends HTMLElement {
+  static config = Symbol('config')
+  static css = new CSSStyleSheet()
+  static {
+    this.css.replace(`
+      monaco-editor {
+        display: block;
+        width: 100%;
+        height: 100%;
+        position: relative;
+      }
+      .editor {
+        display: block;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        inset: 0;
+        visibility: hidden !important;
+      }
+      .editor.active {
+        visibility: visible !important;
+      }`)
+    document.adoptedStyleSheets = [...document.adoptedStyleSheets, this.css]
+  }
+
+  editors = Array.from({ length: 2 }, () => {
+    const el = document.createElement('div')
+    el.classList.add('editor')
+    el.activate = () => {
+      for (const editor of this.editors) {
+        editor === el
+          ? editor.classList.add('active')
+          : editor.classList.remove('active')
+      }
+    }
+    return el
+  })
+  editor
+  diffEditor
+  constructor() {
+    super()
+    if (!globalThis.require) throw Error("Monaco Editor is not loaded. Please include loader.js and configure require paths.")
+    globalThis.require(['vs/editor/editor.main'], () => {
+      this.initEditors()
+      this.dispatchEvent(new Event("ready", { bubbles: true, composed: true }))
+    })
+    setTimeout(() => this.append(...this.editors), 0)
+  }
+
+  initEditors() {
+    this.editor = monaco.editor.create(this.editors[0], {
       theme: "vs-dark",
       automaticLayout: true,
       tabSize: 2,
       insertSpaces: true,
     })
+    this.diffEditor = monaco.editor.createDiffEditor(this.editors[1], {
+      theme: "vs-dark",
+      automaticLayout: true,
+      tabSize: 2,
+      insertSpaces: true,
+      hideUnchangedRegions: { enabled: true },
+    })
+    this.editors[0].activate()
   }
 
   static observedAttributes = ['language', 'target', 'diff']
@@ -46,8 +84,8 @@ customElements.define("monaco-editor", class MonacoEditor extends MonacoElement 
     if (name === 'target') {
       const target = document.querySelector(newValue)
       if (!target) throw Error(`Target element not found: ${newValue}`)
-      let config = target[MonacoElement.config]
-      if (!config) config = target[MonacoElement.config] = {}
+      let config = target[MonacoEditor.config]
+      if (!config) config = target[MonacoEditor.config] = {}
       let language = config.language
       let model = config.model
       if (!model) model = config.model = monaco.editor.createModel(target.value, language)
@@ -56,11 +94,14 @@ customElements.define("monaco-editor", class MonacoEditor extends MonacoElement 
 
       let viewState = config.viewState
       if (viewState) this.editor.restoreViewState(viewState)
-      this.editor.focus()
+      this.editors[0].activate()
       return
     }
     if (name === 'diff') {
+      if (!this.getAttribute('diff')) return this.editors[0].activate()
+      this.editors[1].activate()
       console.log('diff attribute changed:', newValue)
+      // 後はここだけ。。。
       return
     }
   }
@@ -72,26 +113,5 @@ customElements.define("monaco-editor", class MonacoEditor extends MonacoElement 
     value == null
       ? this.removeAttribute("language")
       : this.setAttribute("language", value)
-  }
-})
-
-/**
- * Monaco Diff Editor Web Component
- * <script src="vendor/vs/loader.js"></script>
- * <script>require.config({ paths: { vs: "vendor/vs" } })</script>
- * <script src="js/monaco-editor.js"></script>
- * 
- * <monaco-diff-editor></monaco-diff-editor>
- * <monaco-diff-editor language="json"></monaco-diff-editor>
- */
-customElements.define("monaco-diff-editor", class MonacoDiffEditor extends MonacoElement {
-  async initialize() {
-    this.editor = monaco.editor.createDiffEditor(this, {
-      theme: "vs-dark",
-      automaticLayout: true,
-      tabSize: 2,
-      insertSpaces: true,
-      hideUnchangedRegions: { enabled: true },
-    })
   }
 })
