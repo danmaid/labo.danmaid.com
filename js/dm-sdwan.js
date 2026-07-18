a4213ffe0f240b49cf39026871c3a536b74523c2
yamada@danmaid.com <yamada@danmaid.com>
2026-07-18 13:54:11 +0900

    sdwan terminal

--- js/dm-sdwan.js
+++ js/dm-sdwan.js
@@ -54,4 +54,109 @@ customElements.define('dm-sdwan', class DmSdwan extends HTMLElement {
       credentials: 'include'
     }).then(r => r.text())
   }
+
+  /**
+   * example:
+   * ```
+   * const r = await sdwan.ssh('1.1.1.1')
+   * const socket = new SdwanSshSocket(r.url) // like WebSocket
+   * ```
+   */
+  async ssh(systemIp) {
+    const r = await this.fetch(`/newssh/connection/${systemIp}`, {
+      method: 'POST',
+      body: JSON.stringify({
+        username: 'dummy',
+        password: 'dummy',
+        height: 1000,
+        width: 945.33
+      })
+    })
+    if (!r.ok) throw r
+    return await r.json()
+  }
 })
+
+// ShellInABoxConnector
+class SdwanSshSocket extends EventTarget {
+  constructor(url) {
+    super()
+    const u = new URL(url)
+    this.url = u.origin + u.pathname
+    this.params = u.searchParams
+    this.params.set('width', 100)
+    this.params.set('height', 30)
+    this.abort = new AbortController()
+    this.open().then(() => this.poll())
+      .catch(err => this.dispatchEvent(new ErrorEvent('error', { error: err })))
+  }
+
+  async open() {
+    const body = new URLSearchParams(this.params)
+    body.set('rooturl', this.url)
+    const r = await fetch(this.url, {
+      method: 'POST',
+      body,
+      credentials: 'include',
+      signal: this.abort.signal,
+    })
+    if (!r.ok) throw new Error(`HTTP ${r.status}`)
+    const { session } = await r.json()
+    this.params.set('session', session)
+    this.dispatchEvent(new Event('open'))
+  }
+
+  close() {
+    this.abort.abort()
+  }
+
+  async poll() {
+    while (!this.abort.signal.aborted) {
+      try {
+        const data = await this.receive()
+        if (data) this.dispatchEvent(new MessageEvent('message', { data }))
+      } catch (err) {
+        if (err?.name === 'TimeoutError') continue
+        if (err?.name === 'AbortError') break
+        this.dispatchEvent(new ErrorEvent('error', { error: err }))
+        this.close()
+      }
+    }
+  }
+
+  async receive() {
+    const r = await fetch(this.url, {
+      method: 'POST',
+      body: this.params,
+      credentials: 'include',
+      signal: AbortSignal.any([this.abort.signal, AbortSignal.timeout(30000)])
+    })
+    if (!r.ok) throw new Error(`HTTP ${r.status}`)
+    const d = await r.json()
+    return d.data
+  }
+
+  queue = Promise.resolve()
+
+  send(data) {
+    return this.queue = this.queue
+      .catch(() => { })
+      .then(() => this._send(data))
+  }
+
+  async _send(data) {
+    let hex = ''
+    for (let i = 0; i < data.length; i++) {
+      hex += data.charCodeAt(i).toString(16).toUpperCase().padStart(2, '0')
+    }
+    const body = new URLSearchParams(this.params)
+    body.set('keys', hex)
+    const r = await fetch(this.url, {
+      method: 'POST',
+      body,
+      credentials: 'include',
+      signal: this.abort.signal
+    })
+    if (!r.ok) throw new Error(`HTTP ${r.status}`)
+  }
+}
